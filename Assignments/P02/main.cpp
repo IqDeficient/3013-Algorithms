d9bea7f19fedfd5dc8821c7c4f34ff773888fbbc
Trey Taylor <123134793+IqDeficient@users.noreply.github.com>
2024-02-20 23:06:24 -0600

    my PC

---
+++ Assignments/P02/main.cpp
@@ -0,0 +1,17 @@
+/*****************************************************************************
+*
+*  Author:           Trey Taylor
+*  Email:            Tbtaylor0816@my.msutexas.edu
+*  Label:            P02
+*  Title:            Autocomplete in Linear Time
+*  Course:           3013 Algorithms
+*  Semester:         Spring 2024
+*
+*  Description:
+*        This program will autocomplete a word based on a few characters given
+*        to it by the user.
+*
+*  Files:            
+*       main.cpp
+*****************************************************************************/
+
